aa39ad694ad6a92a72b18e4bf3b3f60909afc1d2
Aayush Sharma <sharma31aayush@gmail.com>
2022-08-07 14:00:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -94,41 +94,4 @@ btnHold.addEventListener('click', function () {
 });
 
 btnNew.addEventListener('click', init);
-// 'use strict';
 
-// const score0El = document.querySelector('#score--0');
-// const score1El = document.getElementById('score--1');
-// const current0El = document.getElementById('current--0');
-// const current1El = document.getElementById('current--1');
-// const diceEl = document.querySelector('.dice');
-// const btnNew = document.querySelector('.btn--new');
-// const btnRoll = document.querySelector('.btn--roll');
-// const btnHold = document.querySelector('.btn--hold');
-// //starting conditions
-// score0El.textContent = 0;
-// score1El.textContent = 0;
-// diceEl.classList.add('hidden');
-
-// const score = [0, 0];
-// let currentScore = 0;
-// let activePlayer = 0;
-
-// //Rolling dice funcionality
-// btnRoll.addEventListener('click', function () {
-//   //generating randon dice roll
-//   const dice = Math.trunc(Math.random() * 6) + 1;
-//   console.log(dice);
-//   //show dice
-//   diceEl.classList.remove('hidden');
-//   diceEl.src = `dice-${dice}.png`;
-
-//   //if dice rolls 1
-//   if (dice !== 1) {
-//     currentScore += dice;
-//     document.getElementById(`current-${activePlayer}`).textContent =
-//       currentScore;
-//     current0El.textContent = currentScore;
-//   } else {
-//     activePlayer = activePlayer === 0 ? 1 : 0;
-//   }
-// });
